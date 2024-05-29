3a843b3dc95d4beb9a9e30dd8d8b5b44d1809be7
Razig17 <abdelrazig.sharif1@gmail.com>
2024-05-30 02:23:13 +0300

    Update revies module to use DBstorage

--- models/place.py
+++ models/place.py
@@ -2,6 +2,8 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 
 class Place(BaseModel, Base):
@@ -18,3 +20,4 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    reviews = relationship("Review", backref="place")
