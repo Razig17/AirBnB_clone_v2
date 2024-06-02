5f08125aceea93ea5645705e6ff2fda62d9872d5
Razig17 <abdelrazig.sharif1@gmail.com>
2024-06-03 00:18:22 +0300

    Task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def tear_down():
+    """Close the current session"""
+    storage.close()
+
+
+@app.route("/states_list")
+def states_list():
+    """Display a HTML page with a list of all the states"""
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
